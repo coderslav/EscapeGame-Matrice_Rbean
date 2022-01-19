1c7aa27c30f20775bf084fac7a971e3bb8383864
rkomin <kominslav@gmail.com>
2022-01-19 18:53:43 +0100

    DB changed for CASCADE Deleting/Updating

--- db/migrations/20211030201022-create-slot.js
+++ db/migrations/20211030201022-create-slot.js
@@ -1,28 +1,37 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('slots', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      during: {
-        type: Sequelize.RANGE(Sequelize.DATE),
-        allowNull: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('slots');
-  }
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('slots', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            during: {
+                type: Sequelize.RANGE(Sequelize.DATE),
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            roomId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'rooms', // name of Target table
+                    key: 'id', // key in Target model that we're referencing
+                },
+                onUpdate: 'CASCADE',
+                onDelete: 'SET NULL',
+            },
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('slots');
+    },
 };
